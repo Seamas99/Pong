cc5602d6f3b77bbd94be11cd305cbedb6ca97e06
Seamas99 <mclaughlin-s65@ulster.ac.uk>
2024-02-18 13:02:54 +0000

    Add project files.

---
+++ Pong/PongUtil.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pong
+{
+    public static class PongUtil
+    {
+        public static void chooseControls()
+        {
+            Console.WriteLine("Left Player\nPlease choose an up key:");
+            char leftUpKey = (char)Console.ReadKey().Key;
+            Console.WriteLine("");
+            while (!confirmOption())
+            {
+                Console.WriteLine("Left Player\nPlease choose an up key:");
+                leftUpKey = (char)Console.ReadKey().Key;
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Left Player\nPlease choose a down key:");
+            char leftDownKey = (char)Console.ReadKey().Key;
+            Console.WriteLine("");
+            while (!confirmOption())
+            {
+                Console.WriteLine("Left Player\nPlease choose a down key:");
+                leftDownKey = (char)Console.ReadKey().Key;
+                Console.WriteLine("");
+            }
+
+
+            Console.WriteLine("Right Player\nPlease choose an up key:");
+            char rightUpKey = (char)Console.ReadKey().Key;
+            Console.WriteLine("");
+            while (!confirmOption())
+            {
+                Console.WriteLine("Right Player\nPlease choose an up key:");
+                rightUpKey = (char)Console.ReadKey().Key;
+                Console.WriteLine("");
+            }
+            Console.WriteLine("Right Player\nPlease choose a down key:");
+            char rightDownKey = (char)Console.ReadKey().Key;
+            Console.WriteLine("");
+            while (!confirmOption())
+            {
+                Console.WriteLine("Right Player\nPlease choose a down key:");
+                rightDownKey = (char)Console.ReadKey().Key;
+                Console.WriteLine("");
+            }
+        }
+
+        public static bool confirmOption()
+        {
+            Console.WriteLine("Please confirm y/n");
+            confirm:;
+            string option = Console.ReadLine();
+
+            if (option != null && option.Length > 0)
+            {
+                if (option.Equals("y"))
+                {
+                    return true;
+                }
+                else if (option.Equals("n"))
+                {
+                    return false;
+                }
+                else
+                {
+                    while (option != "y" || option != "n")
+                    {
+                        Console.WriteLine("Invalid option! Please enter y/n");
+                        option = Console.ReadLine();
+                        if (option.Equals("y"))
+                        {
+                            return true;
+                        }
+                        else if (option.Equals("n"))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Invalid option! Please enter y/n");
+                goto confirm;
+            }
+            return false;
+        }
+
+        public static bool checkKeyMappings(char keyCharacter, string whichKey)
+        {
+            if (whichKey.Equals("leftUpKey"))
+            {
+
+            } else if (whichKey.Equals("leftDownKey"))
+            {
+
+            } else if (whichKey.Equals("rightUpKey"))
+            {
+
+            } else if (whichKey.Equals("rightDownKey"))
+            {
+
+            }
+            return false;
+        }
+    }
+}
---
+++ Pong/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Pong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            const int tennisCourtLength = 50, tennisCourtWidth = 20;
+            PongUtil.chooseControls();
+            const char courtOutline = '-';
+            string outline = string.Concat(Enumerable.Repeat(courtOutline, tennisCourtLength));
+
+            int racketSize = tennisCourtWidth / 4; // racket size will be 1/4 of tennis court width
+            char racket = '|';
+            int leftRacketPosition = 0;
+            int rightRacketPosition = 0;
+            while (true)
+            {
+                Console.SetCursorPosition(0, 0); //top outline
+                Console.WriteLine(outline);
+                Console.SetCursorPosition(0, tennisCourtWidth); //bottom outline
+                Console.WriteLine(outline);
+
+                for (int i = 0; i < racketSize; i++) //print rackets
+                {
+                    Console.SetCursorPosition(0, i + 1 + leftRacketPosition); //left racket
+                    Console.WriteLine(racket);
+                    Console.SetCursorPosition(tennisCourtLength - 1, i + 1 + rightRacketPosition); //right racket
+                    Console.WriteLine(racket);
+                } // end of racket print loop
+
+                switch(Console.ReadKey().Key) { 
+                
+                }
+            }//end of game loop
+        }
+    }
+}
